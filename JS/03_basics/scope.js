60cdbcedddc54c52216cb5af763b630102ad558a
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-04-08 15:17:14 +0530

    scope understanding in js

---
+++ JS/03_basics/scope.js
@@ -0,0 +1,32 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+// var c = 300
+
+// if (true){
+//     let a = 10
+//     const b = 20
+//     var c = 30
+//     c = 300
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+if (true){
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a);
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+console.log(a)
+
+
