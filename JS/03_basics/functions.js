af2dae8785a083d1d84117c535a8bb03e4c856dc
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-04-08 15:08:11 +0530

    function part 2

--- JS/03_basics/functions.js
+++ JS/03_basics/functions.js
@@ -78,3 +78,48 @@ console.log(loginUserMessage())
 console.log(loginUserMessage("hitesh"))
 
 
+// function part 2
+
+// function calculateCartPrice(num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200, 400, 500))
+
+
+// function calculateCartPrice(...num1){ // Rest operator(...)
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username:"sam",
+    price:399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue[200, 400, 500, 1000]);
