c48785615b542e77dcfa65f64197b739b17b38ff
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-04-08 07:30:45 +0530

    just saved the datatype conversion file

--- JS/01_basics/datatypeConversion.js
+++ JS/01_basics/datatypeConversion.js
@@ -10,16 +10,17 @@ console.log(valueInNumber);
 // "33" => 33
 // NaN - Not a Number
 
-"33abc" => NaN
+// "33abc" => NaN
+
 
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
 
-//
-//
-//
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
 
 let someNumber = 33
 let stringNumber = String(someNumber)
