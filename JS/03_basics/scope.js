05de0d5bac2825c9e1277fa1e6c9887207b3d87e
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-04-08 15:37:55 +0530

    scope level and mini hoisting discussion

--- JS/03_basics/scope.js
+++ JS/03_basics/scope.js
@@ -30,3 +30,49 @@ for (let i = 0; i < array.length; i++) {
 console.log(a)
 
 
+
+// scope level and mini hoisting
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website)
+}
+
+// console.log(username)
+
+
+// +++++++++++++++++ Interesting ++++++++++++++++++++
+
+console.log(addone (5));
+
+function addone (num){
+    return num + 1
+}
+
+
+console.log(addone (5));
+
+const addTwo = function(num){
+    return num + 2
+}
+
+
+
