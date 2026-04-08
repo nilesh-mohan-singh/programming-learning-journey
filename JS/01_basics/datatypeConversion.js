f3dd1887ba546c0a09a3e18c50bd0c7fddf61c8a
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-04-08 07:25:36 +0530

    datatype conversion basics

---
+++ JS/01_basics/datatypeConversion.js
@@ -0,0 +1,30 @@
+let score = 33
+
+console.log(typeof score)
+console.log(typeof (score));
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// NaN - Not a Number
+
+"33abc" => NaN
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//
+//
+//
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
