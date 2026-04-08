a471a6fadcb213deac656e5ce45531a71d94da45
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-04-08 09:59:45 +0530

    Stack and Heap memory understanding

--- JS/01_basics/datatypes-summary.js
+++ JS/01_basics/datatypes-summary.js
@@ -43,3 +43,29 @@ console.log(typeof bigNumber);
 console.log(typeof outsideTemp);
 console.log(scoreValue);
 console.log(typeof myFunction);
+
+
+// ***************************************
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename)
+console.log(anothername)
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
