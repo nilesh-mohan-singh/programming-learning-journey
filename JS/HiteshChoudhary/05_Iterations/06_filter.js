f73521207a912d1e9f260d13120f0310974c4b92
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-04-10 00:42:06 +0530

    filter, map and reduce methods

---
+++ JS/HiteshChoudhary/05_Iterations/06_filter.js
@@ -0,0 +1,45 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const balues = coding.forEach((item) => {
+//     // console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// // myNums.filter( (num) => num > 4)
+// myNums.filter( (num) => {
+//     return num > 4
+// })
+
+// console.log(newNums);
+
+
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNums = []
+
+// myNums.forEach((num) => {
+//     if (num > 4) {
+//         newNums.push(num);
+//     }
+// })
+
+// console.log(newNums);
+
+
+
+// const books = [{}, {}, {}, {}, {}, {}]
+// // const userBooks = books.filter((bk) => bk.genre === 'History')
+
+// let userBooks = books.filter((bk) => {bk.publish >= 2000})
+// // userBooks = books.filter((bk) => {return bk.publish >= 1995})
+// userBooks = books.filter((bk) => {
+//     return bk.publish >= 1995 && bk.genre === "History"
+// })
+
+// console.log(userBooks);
+
---
+++ JS/HiteshChoudhary/05_Iterations/08_reduce.js
@@ -0,0 +1,40 @@
+const myNums = [1, 3, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval)) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce(function (acc, currva) {
+    console.log(`acc: ${acc} and currval: ${currval}`);
+    return acc + currval
+}, 0)
+
+// const myTotal = myNums.reduce((acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "python",
+        price: 999
+    }
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    }
+    {
+        itemName: "data science course",
+        price: 2999
+    }
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+console.log(priceToPay)
