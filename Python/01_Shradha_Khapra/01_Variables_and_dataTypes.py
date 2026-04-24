700225e98eaba4d92e4e7e832d95ca4bab426c1e
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-04-24 17:47:06 +0530

    Variables, Datatypes, and operators in Pyton with practice questions

---
+++ Python/01_Shradha_Khapra/01_Variables_and_dataTypes.py
@@ -0,0 +1,163 @@
+# print("Hello World")
+# print("My name is Nilesh")
+# print("Hi, I have started exploring python from today")
+# print('''we'll soon be comfortable in python, if we keep practicing''')
+
+
+# print("Shradha is my name.")
+# print("My age is 23.")
+# print("Shradha is my name", "My age is 23")
+
+# print(23)
+# print(23 + 35)
+
+# name = "shradha"
+# age = 23
+# age2 = age
+# price = 25.99
+
+# print("name")
+# print(name)
+# print(age)
+# print(price)
+# print("my name is:", name)
+# print("my age is:", age)
+# print(age2)
+
+# name = "Shradha"
+# age = 25
+# price = 25.99
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+
+# name1 = "sk"
+# name2 = 'sk'
+# name3 = '''sk'''
+
+# print(name1)
+# print(name2)
+# print(name3)
+
+
+
+# ***********Data Types************
+# Integers 
+# Strings
+# Float
+# Boolean
+# None
+
+
+
+# age = 23
+# old = True
+# a = None
+# print(type(old))
+# print(type(a))
+
+
+# Keywords
+# Keywords are reserved words in python
+# Python is case-sensitive language
+
+
+
+# Print Sum
+# """
+# a = 2
+# b = 5
+# sum = a+b
+# print(sum)
+# """
+
+
+
+# Arithmetic Operators (+, -, *, /, %, **)
+# Relational/Comparison Operators ( ==, !=, >, <, >=, <= )
+# Assignment Operators ( =, +=, -=, *=, /=, %=, **=)
+# Logical Operators (not, and, or)
+
+
+# Type Conversion
+# Type Casting
+
+
+# Type Conversion
+# a = 1
+# b = 2.0
+# sum = a + b
+# print(sum)
+
+
+# Type Casting
+# a = 1
+# b = "2"
+# c = int(b)
+# sum = a + c
+# print(sum)
+
+
+
+# Input in Python\
+# Input() statement is used to accept values (using Keyboard) from user
+
+# input()  # result for input() is always a str
+# int(input()) # int
+# float(input()) #float
+
+# name = input("enter name:")
+# age = int(input("enter age:"))
+# marks = float(input("enter marks:"))
+
+# print("welcome", name)
+# print("age =", age)
+# print("marks =", marks)
+
+
+
+"""
+Let's Practice
+===============
+
+1. Write a Program to input 2 numbers & print their sum
+
+2. Write a Proggram to input sidde of a square & print its area.
+
+3. WAP to input 2 floating pint numbers & print their avverage.
+
+4. WAP to innput 2 int numbers, a and b. Print True if a greater than or equal to b. 
+If not print False.
+"""
+
+
+# Solution 1
+x = 2
+y = 3
+sum = x + y
+print(sum)
+
+# Solution 2
+side = 4
+area = side * side
+print(area)
+
+# Solution 3
+a = float(2)
+b = 3.2
+avg = (a+b)/2
+print(avg)
+
+
+# Solution 4
+a = int(4)
+b = int(3)
+if(a>b):
+    print("True")
+elif(a==b):
+    print("True")
+else:
+    print("False")
+
